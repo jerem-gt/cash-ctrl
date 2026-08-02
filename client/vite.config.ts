5fd0c8b57f948d5c60b8b30b1c60e4e36f01bc3c
jerem <jerem.gt@proton.me>
2026-08-02 11:01:06 +0200

    fix: build config dirname > import.meta.dirname

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -41,7 +39,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': import.meta.dirname + '/src',
     },
     dedupe: ['react', 'react-dom'],
   },
--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
@@ -18,7 +16,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': import.meta.dirname + '/src',
     },
   },
   test: {
